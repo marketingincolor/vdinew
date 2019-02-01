acbc92a99018747a22c640025324c5e2b688e5b9
Edd Twilbeck <etwilbeck@marketingincolor.com>
2019-02-01 09:31:31 -0600

    added analytics and ss

--- wp-content/themes/vdi/header-home.php
+++ wp-content/themes/vdi/header-home.php
@@ -48,11 +48,21 @@
 		<!-- End Sharpspring tracking -->
 
 		<?php wp_head(); ?>
-
+		<!-- Google Tag Manager -->
+		<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+		new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+		j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+		'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+		})(window,document,'script','dataLayer','GTM-MNCV9C9');</script>
+		<!-- End Google Tag Manager -->
+		
 	</head>
 			
 	<body <?php body_class(); ?>>
-
+		<!-- Google Tag Manager (noscript) -->
+		<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MNCV9C9"
+		height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+		<!-- End Google Tag Manager (noscript) -->
 		<div id="mobile-left-menu" class="mobile-left-menu absolute">
 			<div class="menu-container relative">
 				<button class="absolute" aria-label="<?php _e( 'Close Menu', 'madx' ); ?>" type="button"><i class="fal fa-long-arrow-left"></i></button>
